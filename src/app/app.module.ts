bd9d1508b5ff7f7fa1509704bf1af4259c861e7b
Brandon Potts <ObeliskIsHuge@gmail.com>
2018-10-24 14:59:56 -0400

    added section-body component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SectionBodyComponent } from './section-body/section-body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SectionBodyComponent
   ],
   imports: [
     BrowserModule,
