32ed20f834a095d30d49901261f916b32c58c6f4
Brandon Potts <ObeliskIsHuge@gmail.com>
2018-10-30 14:22:16 -0400

    html templates are now able to to be swapped.

--- src/app/section-body/section-body.component.ts
+++ src/app/section-body/section-body.component.ts
@@ -1,9 +1,10 @@
 import {Component, ComponentFactoryResolver, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 
-// import {AboutComponent} from './body-components/about.component';
 import {SwapDirective} from './utils/swap.directive';
 import {BodyItem} from './utils/body-item';
 import {BodyService} from './utils/body.service';
+import {ActivatedRoute, UrlSegment} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-section-body',
@@ -16,9 +17,11 @@ export class SectionBodyComponent implements OnInit, OnDestroy {
 
   @Input() components: BodyItem[];
   @ViewChild(SwapDirective) bodyHost: SwapDirective;
+  currentURL: Observable<UrlSegment[]>;
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver, private bodyService: BodyService) {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private bodyService: BodyService, route: ActivatedRoute) {
     this.components = bodyService.getBody();
+    this.currentURL = route.url;
   }
 
   ngOnInit() {
@@ -32,12 +35,25 @@ export class SectionBodyComponent implements OnInit, OnDestroy {
   }
 
   loadComponent() {
-    console.log(`This is the body Items Array: ` + this.components);
-    const bodyItem = this.components[0];
+
+    let currentRoute: String;
+    this.currentURL.subscribe(value => currentRoute = value.toString());
+
+    let bodyItem;
+    if (currentRoute === '') {
+      bodyItem = this.components[0];
+    } else if (currentRoute === 'about') {
+      bodyItem = this.components[1];
+    } else if (currentRoute === 'experience') {
+      bodyItem = this.components[2];
+    } else if (currentRoute === 'projects') {
+      bodyItem = this.components[3];
+    } else {
+      // TODO Need to have a default view
+    }
     const componentFactory = this.componentFactoryResolver.resolveComponentFactory(bodyItem.component);
     const viewContainerRef = this.bodyHost.viewContainerRef;
     viewContainerRef.clear();
-
     const componentRef = viewContainerRef.createComponent(componentFactory);
     componentRef.instance.data = bodyItem.data;
   }
